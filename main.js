210f7e3f872e579cf6464529f09b160dcda92afe
ka-yuya-ponta <kami.yu.goal@icloud.com>
2021-09-08 19:54:35 +0900

    first

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 
 
+
+
+  //////////// 一番目以外のコンテンツは非表示
+  $(".menu-box:not(:first-of-type)").css("display", "none");
+
+  //////////// タブの制御
+  $('.tab').click( function() { // タブメニューをhoverしたら
+    var index = $('.tab').index(this); // hoverしたタブ番号を取得
+    $('.tab').removeClass('current'); // タブ現在地クラスを削除し、
+    $(this).addClass('current'); // hoverしたタブにタブ現在地クラスを付与
+
+    //////////// コンテンツの制御
+    $('.menu-box').hide().eq(index).show(); // hoverしてないコンテンツは非表示、hoverした番号は表示
+  });
